e9f02d79cbdb789ecac0d9ebf6c86178058b4f30
Hentai Ousama <kutabare.eerito@gmail.com>
2022-01-23 17:53:13 +0530

    Base Framework Updated

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
-# Quizzer
+<p align="center">
+  <img src="./Quizzer-Info-Img.jpg" alt="Information about Quizzer">
+</p>
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.4.
+<h1 align="center">Quizzer</h1>
+
+Base framework for the project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 13.1.1.
+
+## Initialization
+
+Following environment variables need to be set up before using this project: -
+
+<label>For server: -</label>
+<ul>
+  <li><code>PORT</code></li>
+</ul>
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+The root folder contains a `server.js` file.
 
-## Code scaffolding
+<ul>
+  <li>Step <code>0</code>: Make sure all node dependencies are already installed.</li>
+  <li>Step <code>1</code>: Build the angular app.</li>
+  <li>Step <code>2</code>: Set the environment variable <code>PORT</code> on which you want the application to listen on (default is <code>6970</code>).</li>
+  <li>Step <code>3</code>: Run <code>node server</code> to start the server.</li>
+</ul>
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+Application can then be visited on `localhost:PORT`.
 
 ## Build
 
@@ -17,11 +35,3 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
---
+++ server.js
@@ -0,0 +1,94 @@
+"use strict";
+
+const startTime = Date.now();
+
+const winston = require('winston');
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: {service: 'user-service'},
+  transports: [
+    // - Write all logs with level `info` and below to `combined.log`
+    new winston.transports.File({filename: './Logs/jsLog.log'})
+  ],
+});
+
+// If we're not in production then log to the `console` with the format: `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+if (process.env["NODE_ENV"] !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+Object.freeze(logger);
+
+// Putting it in global object so that it can be accessed from anywhere.
+// This practice should be avoided and be used in cases only like this.
+global["globalLoggerObject"] = logger;
+
+const express = require('express');
+const http = require('http');
+const {Server} = require('socket.io');
+const uuid = require('uuid');
+const angularJson = require('./angular.json');
+
+let portNumber = process.env["PORT"];
+if (!portNumber) {
+  portNumber = 6970;
+}
+
+const app = express();
+const httpServer = http.createServer(app);
+const outputFolder = __dirname + "/" + angularJson["projects"]["Quizzer"]["architect"]["build"]["options"]["outputPath"];
+
+// Shutdown Handler
+const shutdownHandler = (event) => {
+  logger.info("Shutdown Handler Start for " + event);
+  // TODO : Do something...
+
+  setTimeout(() => {
+    logger.info("Shutdown Handler End");
+    process.exit(1);
+  }, 25000);
+};
+if (process.stdin.isTTY && !process.stdin.isRaw) {
+  process.stdin.setRawMode(true);
+}
+process.on("SIGINT", shutdownHandler);
+process.on("SIGTERM", shutdownHandler);
+process.stdin.resume();
+
+const io = new Server(httpServer);
+
+// --- SERVE NODE MODULES --- //
+app.get('/node_modules/*.*', (req, res) => {
+  try {
+    res.status(200).sendFile(__dirname + req.path);
+  } catch {
+    res.sendStatus(404);
+  }
+});
+
+// ---- SERVE STATIC FILES ---- //
+app.get('*.*', express.static(outputFolder, {maxAge: '3d'}));
+
+// ---- SERVE APPLICATION PATHS ---- //
+app.all('*', function (req, res) {
+  res.status(200).sendFile(`/`, {root: outputFolder});
+});
+
+let activeUsers = 0;
+
+io.on('connection', (socket) => {
+  activeUsers += 1;
+
+  socket.on('disconnect', () => {
+    activeUsers -= 1;
+  });
+});
+
+const endTime = Date.now();
+logger.info("Initialization Complete in " + (endTime - startTime) / 1000 + " seconds");
+
+httpServer.listen(portNumber, () => {
+  logger.info('Listening on port ' + process.env.PORT);
+});
